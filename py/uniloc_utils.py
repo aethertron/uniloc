407460c0332544a820f1999d2ba3ea67eab18c97
kafkabro <william.stubler@gmail.com>
2016-11-06 23:35:30 -0700

    newlines

--- py/uniloc_utils.py
+++ py/uniloc_utils.py
@@ -5,6 +5,7 @@ from __future__ import print_function
 import sys
 import json
 
+
 def po(obj, file=sys.stdout, prune_private=True, prune_protected=True):
     keys = dir(obj)
     if prune_private:
@@ -13,6 +14,7 @@ def po(obj, file=sys.stdout, prune_private=True, prune_protected=True):
         keys = [key for key in keys if not key.startswith('_')]
     dictionary = {key: getattr(obj, key) for key in keys}
     json.dump(dictionary, file, indent=1, default=lambda x: str(x)[:24])
+
 
 def pd(obj, file=sys.stdout):
     json.dump(dir(obj), file, indent=1, default=str)
