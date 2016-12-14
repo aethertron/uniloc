46661e14620d38bc65fbb8eb44499e1cad059f59
kafkabro <william.stubler@gmail.com>
2016-12-14 00:49:23 -0700

    added vert

---
+++ py/vert.py
@@ -0,0 +1,22 @@
+#!/usr/bin/python2
+from __future__ import print_function
+import uniloc_utils
+#
+import argparse
+import sys
+
+
+def main(args):
+    parser = argparse.ArgumentParser(parents=[uniloc_utils.argparse_filter_parser()])
+    args = parser.parse_args(args)
+    vals = args.fin.read()
+    if ',' in vals:
+        sep = ','
+    else:
+        sep = None
+    vals = vals.split(sep)
+    for val in vals:
+        print(val.strip(), file=args.fout)
+
+if __name__ == '__main__':
+    main(sys.argv[1:])
