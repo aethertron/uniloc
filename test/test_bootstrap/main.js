e2468352e115037645c12ac45bd8ef475614d3e3
nooSphere <william.stubler@gmail.com>
2017-02-20 13:17:24 -0700

    added test files

---
+++ test/test_bootstrap/main.js
@@ -0,0 +1,7 @@
+$(document).ready(function() {
+    console.log("Fully loaded.");
+    // $('#modal-submit').click(function() {
+    // 	$('#myModal').modal('modal');
+    // 	return false;
+    // })
+})
